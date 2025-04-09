c7cf741af94bd9e59802496995b37d4bce5b5e96
Gerald Metohu <geraldmetohu@gmail.com>
2025-04-09 16:32:27 +0100

    Clean layout.tsx and remove unused dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import { extractRouterConfig } from "uploadthing/server";
-import { ourFileRouter } from "./api/uploadthing/core";
 
 import { NavBar } from "./componets/storefront/NavBar";
 import { Footer } from "./componets/storefront/Footer";
 
+// Load custom fonts
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,13 +29,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f4f4f4]`}>
-        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
+        {/* Navigation Bar */}
         <div className="fixed top-0 left-0 w-full z-50 bg-[#f4f4f4] shadow transition-all duration-300">
           <NavBar />
         </div>
-        <main className="pt-24 ">
+
+        {/* Main Content */}
+        <main className="pt-24">
           {children}
         </main>
+
+        {/* Footer */}
         <Footer />
       </body>
     </html>
